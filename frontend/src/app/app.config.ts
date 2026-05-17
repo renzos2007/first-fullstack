5426c4d80227cd600798639bde8bba8532f8025d
renzos2007 <Renzo@stelleronline.nl>
2026-05-17 14:56:11 +0200

    bugfix: translation stopped working with update.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,23 +4,23 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './config/interceptors';
-import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {TranslateModule, TranslateLoader, provideTranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader, TranslateHttpLoader} from '@ngx-translate/http-loader';
 
-const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './i18n/', '.json');
+// const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+//   new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom([TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
-      },
-    })])
+    provideTranslateService({
+      defaultLanguage: 'nl',
+      loader: provideTranslateHttpLoader({
+        prefix: './i18n/',
+        suffix: '.json'
+      })
+    })
   ],
 };
